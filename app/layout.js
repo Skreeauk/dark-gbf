276ea28d035a5fbfd555019c7f149732fd54e261
Skreeauk <waldojongsoo@yahoo.com>
2024-05-19 06:28:49 +0800

    Testing MagicUI and AceternityUI

--- app/layout.js
+++ app/layout.js
@@ -1,35 +1,47 @@
 import { Inter } from "next/font/google"
 import "@/css/globals.css"
+
 import { ThemeProvider } from "@/components/providers/ThemeProvider"
+
+import DockBar from "@/components/DockBar"
+
+import { cn } from "@/lib/utils"
 
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata = {
-  title: "Dark GBF",
-  description: "Tools for Dark Players",
+	title: "Dark GBF",
+	description: "Tools for Dark Players",
 }
 
 export default function RootLayout({ children }) {
-  return (
-    <html
-      lang="en"
-      suppressHydrationWarning
-    >
-      <head>
-        <link
-          rel="icon"
-          href="/favicon.ico"
-        />
-      </head>
-      <body className={inter.className}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-        >
-          {children}
-        </ThemeProvider>
-      </body>
-    </html>
-  )
+	return (
+		<html
+			lang="en"
+			suppressHydrationWarning
+		>
+			<head>
+				<link
+					rel="icon"
+					href="/favicon.ico"
+				/>
+			</head>
+			<body
+				className={cn(
+					"antialiased min-h-screen bg-background flex flex-col",
+					inter.className
+				)}
+			>
+				<ThemeProvider
+					attribute="class"
+					defaultTheme="system"
+					enableSystem
+					disableTransitionOnChange
+				>
+					{children}
+					<DockBar />
+				</ThemeProvider>
+			</body>
+		</html>
+	)
 }
--- components/ModeToggle.jsx
+++ components/ModeToggle.jsx
@@ -1,42 +1,41 @@
 "use client"
 
+import { useState, useEffect } from "react"
+
 import { MoonIcon, SunIcon } from "@radix-ui/react-icons"
 import { useTheme } from "next-themes"
 
 import { Button } from "@/components/ui/button"
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
 
 export function ModeToggle() {
-  const { setTheme } = useTheme()
+	const [mounted, setMounted] = useState(false)
+	const { resolvedTheme, setTheme } = useTheme()
 
-  return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button
-          variant="outline"
-          size="icon"
-        >
-          <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
-          System
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
-  )
+	// useEffect only runs on the client, so now we can safely show the UI
+	useEffect(() => {
+		setMounted(true)
+	}, [])
+
+	if (!mounted) {
+		return null
+	}
+
+	return (
+		<Button
+			variant="ghost"
+			size="icon"
+			className="p-2 hover:bg-transparent"
+			onClick={() =>
+				setTheme(
+					resolvedTheme === "dark" ? "light" : "dark"
+				)
+			}
+		>
+			{resolvedTheme === "dark" ? (
+				<MoonIcon className="size-6 text-primary" />
+			) : (
+				<SunIcon className="size-6 text-primary" />
+			)}
+		</Button>
+	)
 }
