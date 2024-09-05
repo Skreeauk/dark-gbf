087088eb17b254cf96769e2bd5db6cc47f1f3de1
Skreeauk <waldojongsoo@yahoo.com>
2024-09-06 05:10:55 +0800

    Grids for basic setups

--- components/mdx/MDXWeaponGrid.jsx
+++ components/mdx/MDXWeaponGrid.jsx
@@ -15,44 +15,62 @@ export async function MDXWeaponGrid({
 		getCachedWeaponSkillsDocs(),
 	])
 
+	const mh = weaponFetch.find((e) => e.id == weapons[0])
+
 	return (
 		<div className="flex flex-col gap-1 mx-auto md:flex-row md:justify-center md:gap-4">
-			<Weapon weaponID={weapons[0]}></Weapon>
+			<Weapon weapon={mh} weaponSkillFetch={weaponSkillFetch}></Weapon>
 
 			<div className="grid grid-cols-3 gap-1 md:gap-4">
-				{weapons?.slice(1).map((weaponID, i) => (
-					<Weapon
-						weaponID={weaponID}
-						key={i + 1}
-						weaponFetch={weaponFetch}
-						weaponSkillFetch={weaponSkillFetch}
-					></Weapon>
-				))}
+				{weapons?.slice(1).map((weaponID, i) => {
+					const weapon = weaponFetch.find((e) => e.id == weaponID)
+
+					return (
+						<Weapon
+							key={i + 1}
+							weapon={weapon}
+							weaponSkillFetch={weaponSkillFetch}
+						></Weapon>
+					)
+				})}
 			</div>
 		</div>
 	)
 }
 
-function Weapon({ weaponID, weaponFetch = [], weaponSkillFetch = [] }) {
-	const weaponData = weaponFetch.find((e) => e.id == weaponID)
+function Weapon({ weapon, weaponSkillFetch }) {
 	return (
 		<div className="relative h-[48px] w-[84px] bg-gray-500 lg:h-[72px] lg:w-[126px] xl:h-[96px] xl:w-[168px]">
-			{weaponID != 0 && (
+			{weapon && (
 				<Image
-					src={"/weapon/" + weaponID + ".webp"}
+					src={"/weapon/" + weapon?.id + ".webp"}
 					fill
 					sizes="(max-width: 1024px) 84px, (max-width: 1280) 126px, 168px"
-					alt={weaponID}
+					alt={weapon?.id}
 					className="not-prose"
 				/>
 			)}
-			<WeaponSkills />
+			<div className="absolute bottom-0 left-0 flex items-center justify-center w-full gap-1 h-2/5 bg-transparent/40">
+				{weapon?.skills &&
+					weapon?.skills.map((skillID, i) => {
+						const skill = weaponSkillFetch.find((e) => e.id == skillID)
+						return <WeaponSkills key={i} skill={skill} />
+					})}
+			</div>
 		</div>
 	)
 }
 
-function WeaponSkills({ weaponID }) {
+function WeaponSkills({ skill }) {
 	return (
-		<div className="absolute bottom-0 left-0 flex flex-row items-center justify-center w-full gap-1 h-1/2 bg-transparent/40 md:h-2/5"></div>
+		<div className={"relative rounded-lg size-5 md:size-10"}>
+			<Image
+				src={"/weapon_skill/grid/" + skill.icon_path + ".webp"}
+				fill
+				sizes="(max-width: 768px) 20px, 40px"
+				alt={skill.icon_path}
+				className="not-prose"
+			/>
+		</div>
 	)
 }
